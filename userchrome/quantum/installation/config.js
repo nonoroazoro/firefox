a16adce109aeb4557a69b60ee81aaa00eb526ee7
Nonoroazoro <xiaochao.k@gmail.com>
2021-01-04 16:27:35 +0800

    refactor

--- userchrome/quantum/installation/config.js
+++ userchrome/quantum/installation/config.js
@@ -1,7 +1,7 @@
 // 2019-12-11 fix 72 use "load" in config.js, working with Sub-Script/Overlay Loader v3.0.60mod
 try
 {
-    let { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+    const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
     Cu.import("resource://gre/modules/Services.jsm");
     Cu.import("resource://gre/modules/osfile.jsm");
@@ -12,33 +12,33 @@ try
     };
 
     UserChrome_js.prototype = {
-        observe: function (aSubject, aTopic, aData)
+        observe(aSubject, aTopic, aData)
         {
             aSubject.addEventListener("load", this, true);
         },
 
-        handleEvent: function (aEvent)
+        handleEvent(aEvent)
         {
-            let document = aEvent.originalTarget;
-            if (document.location && document.location.protocol == "chrome:")
+            const document = aEvent.originalTarget;
+            if (document.location && document.location.protocol === "chrome:")
             {
-                let file = Services.dirsvc.get("UChrm", Ci.nsIFile);
+                const file = Services.dirsvc.get("UChrm", Ci.nsIFile);
                 file.append("userChrome.js");
-                let fileURL = Services.io.getProtocolHandler("file")
+                const fileURL = Services.io.getProtocolHandler("file")
                     .QueryInterface(Ci.nsIFileProtocolHandler)
                     .getURLSpecFromFile(file) + "?" + file.lastModifiedTime;
                 Services.scriptloader.loadSubScript(fileURL, document.defaultView, "UTF-8");
             }
-        },
+        }
     };
 
     if (!Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime).inSafeMode)
     {
         new UserChrome_js();
     }
-} catch (ex) { };
+} catch { };
 
 try
 {
     pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-} catch (e) { }
+} catch { }
